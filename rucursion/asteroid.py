9695186ce6c0e9959cc3dc07aa98b3724ecffd54
Amaz <worachanon226@hotmail.com>
2022-09-01 22:30:11 +0700

    Update asteroid

--- rucursion/asteroid.py
+++ rucursion/asteroid.py
@@ -1,6 +1,25 @@
 def asteroid_collision(asts):
     
-    return str(asts)
+    def recur(n,l):
+        if n == len(asts):
+            return 
+        if len(l) == 0:
+            l.append(asts[n])
+        elif asts[n] < 0 and l[-1] > 0:
+            if abs(asts[n]) == abs(l[-1]):
+                l.pop()
+            elif abs(asts[n]) > l[-1]:
+                l.pop()
+                recur(n, l)
+                return 
+        else:
+            l.append(asts[n])
+        recur(n+1,l)
+    
+    l = []
+    recur(0,l)
+    return l
+
 x = input("Enter Input : ").split(",")
 x = list(map(int,x))
 print(asteroid_collision(x))
