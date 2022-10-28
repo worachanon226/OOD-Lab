4f5a230e384c8ca9a2e0fc7116c07ec7ae80af36
Amaz <worachanon226@hotmail.com>
2022-10-28 21:00:04 +0700

    Update sorting

--- sorting/3-insertion-sort.py
+++ sorting/3-insertion-sort.py
@@ -1,13 +1,14 @@
-def insertion(l):
-    for i in range(1, len(l)):
-        ele_i = l[i]
-        for j in range(i, -1, -1):
-            if ele_i < l[j-1] and j>0:
-                l[j] = l[j-1]
-            else:
-                l[j] = ele_i
-                break
-        print(l)
+def insertion(l, i, j):
+    n = len(l)
+    if i < n and j >= 0:
+        if l[j] > l[j + 1]:
+            temp = l[j + 1]
+            l[j + 1] = l[j]
+            l[j] = temp
+        insertion(l, i, j - 1)
+    if j == 0:
+        insertion(l, i + 1, i)
+    return l
 
 inp = [int(i) for i in input("Enter Input : ").split()]
-insertion(inp)
+print(insertion(inp,1,0))
