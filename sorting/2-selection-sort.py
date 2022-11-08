b80cf732579003b312e98ce226184363bb6a8242
Amaz <worachanon226@hotmail.com>
2022-11-08 19:34:28 +0700

    Update sorting

--- sorting/2-selection-sort.py
+++ sorting/2-selection-sort.py
@@ -1,13 +1,21 @@
-def selection(l):
-    for last in range(len(l)-1,0,-1):
-        mx, mx_i = l[0], 0
-        for i in range(1,last + 1):
-            if l[i] > mx:
-                mx, mx_i = l[i], i
-        if l[last] != l[mx_i]:
-            l[last], l[mx_i] = l[mx_i], l[last]   
-            print(f'swap {l[mx_i]} <-> {l[last]} : {l}')
+def maxidx( l , i , j ):
+    if i == j:
+        return i
+
+    k = maxidx(l, i + 1, j)
+    return (k if l[i] < l[k] else i)
+
+def selection(l, n):
+    if n == 0:
+        return 
+    k = maxidx(l, 0, n)
+     
+    if k != n:
+        l[k], l[n] = l[n], l[k]
+        print(f'swap {l[k]} <-> {l[n]} : {l}')
+         
+    selection(l, n-1)
 
 inp = [int(i) for i in input("Enter Input : ").split()]
-selection(inp)
+selection(inp,len(inp)-1)
 print(inp)
